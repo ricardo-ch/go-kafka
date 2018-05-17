23ca8df609b1d9d875680977c314febd430f9fcb
Erwan van Peteghem <erwan.vanpeteghem@ricardo.ch>
2018-05-17 16:02:00 +0200

    add prometheus Instrumenting (#8)
    add Prometheus instrumenting

---
+++ instrumenting_test.go
@@ -0,0 +1,25 @@
+package kafka
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Shopify/sarama"
+	"github.com/stretchr/testify/assert"
+)
+
+var testEncodedMessage = []byte{10, 3, 49, 50, 51}
+
+func Test_NewConsumerMetricsService_Should_Return_Success_When_Success(t *testing.T) {
+	// Arrange
+	s := NewConsumerMetricsService("test_ok")
+	h := func(context.Context, *sarama.ConsumerMessage) error { return nil }
+
+	// Act
+	handler := s.Instrumentation(h)
+
+	err := handler(context.Background(), &sarama.ConsumerMessage{Value: testEncodedMessage, Topic: "test-topic"})
+
+	// Assert
+	assert.Nil(t, err)
+}
