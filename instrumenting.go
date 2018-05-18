81c3dcfb975e8ee9dccef8546940975ec5a436ee
Erwan van Peteghem <erwan.vanpeteghem@ricardo.ch>
2018-05-18 18:18:33 +0200

    improve instrumenting

--- instrumenting.go
+++ instrumenting.go
@@ -2,6 +2,7 @@ package kafka
 
 import (
 	"context"
+	"strconv"
 	"time"
 
 	"github.com/Shopify/sarama"
@@ -12,40 +13,29 @@ type ConsumerMetricsService struct {
 	request       *prometheus.CounterVec
 	requestFailed *prometheus.CounterVec
 	latency       *prometheus.SummaryVec
-	appName       string
 }
 
 // NewConsumerMetricsService creates a layer of service that add metrics capability
 func NewConsumerMetricsService(appName string) *ConsumerMetricsService {
 	var c ConsumerMetricsService
-	c.appName = appName
-	fieldKeys := []string{"app_name", "topic"}
+	fieldKeys := []string{"topic", "success"}
 
 	c.request = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Namespace: "kafka",
 			Subsystem: "consumer",
-			Name:      "total_request",
+			Name:      "requests_total",
 			Help:      "Number of requests processed",
 		}, fieldKeys)
 	prometheus.MustRegister(c.request)
-
-	c.requestFailed = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Namespace: "kafka",
-			Subsystem: "consumer",
-			Name:      "total_request_failed",
-			Help:      "Number of requests failed",
-		}, fieldKeys)
-	prometheus.MustRegister(c.requestFailed)
 
 	c.latency = prometheus.NewSummaryVec(
 		prometheus.SummaryOpts{
 			Namespace: "kafka",
 			Subsystem: "consumer",
-			Name:      "request_latency_milliseconds",
+			Name:      "requests_latency_milliseconds",
 			Help:      "Total duration in milliseconds",
-		}, fieldKeys)
+		}, []string{"kafka_topic"})
 	prometheus.MustRegister(c.latency)
 
 	return &c
@@ -56,12 +46,12 @@ func (c *ConsumerMetricsService) Instrumentation(next Handler) Handler {
 	return func(ctx context.Context, msg *sarama.ConsumerMessage) (err error) {
 		// add metrics to this method
 		defer func(begin time.Time) {
-			c.latency.WithLabelValues(c.appName, msg.Topic).Observe(time.Since(begin).Seconds() * 1e3)
-			c.request.WithLabelValues(c.appName, msg.Topic).Inc()
-			// If error is not empty, we add to metrics that it failed
-			if err != nil {
-				c.requestFailed.WithLabelValues(c.appName, msg.Topic).Inc()
+			var success bool
+			if err == nil {
+				success = true
 			}
+			c.latency.WithLabelValues(msg.Topic, strconv.FormatBool(success)).Observe(time.Since(begin).Seconds() * 1e3)
+			c.request.WithLabelValues(msg.Topic).Inc()
 		}(time.Now())
 
 		err = next(ctx, msg)
