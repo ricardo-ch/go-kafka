329aef136c9b3e60f990f7cc63d393f2eedd4cde
luphaz <matthieu.bono@gmail.com>
2019-04-15 17:01:22 +0200

    Allow instrumenting several listener(s)
    In order to allow several listeners to be instrumented we needed to
    initialise the prometheus metrics once and only once
    
    We choose to use [double checked locking](https://en.wikipedia.org/wiki/Double-checked_locking)
    
    We also took this opportunity to expose `group_id` label into metrics
    
    Alternative implementations discarded :
    * use `init` method, because it would have initialised the counter
    even if not used
    * use `prometheus.Register` and propagate error, because it would have
    require to store the initial valid counters anyway (to be able to use
    them to increment the counter as an example)

--- instrumenting.go
+++ instrumenting.go
@@ -3,40 +3,85 @@ package kafka
 import (
 	"context"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/Shopify/sarama"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+var (
+	request      *prometheus.CounterVec
+	latency      *prometheus.SummaryVec
+	requestMutex = &sync.Mutex{}
+	latencyMutex = &sync.Mutex{}
+	metricLabels = []string{"kafka_topic", "success", "group_id"}
+)
+
+type prometheusSummaryVec interface {
+	WithLabelValues(lvs ...string) prometheus.Observer
+}
+
+type prometheusCounterVec interface {
+	WithLabelValues(lvs ...string) prometheus.Counter
+}
+
 // ConsumerMetricsService object represents consumer metrics
 type ConsumerMetricsService struct {
-	request *prometheus.CounterVec
-	latency *prometheus.SummaryVec
+	request prometheusCounterVec
+	latency prometheusSummaryVec
+	groupID string
+}
+
+func getPrometheusRequestInstrumentation() prometheusCounterVec {
+	if request != nil {
+		return request
+	}
+
+	requestMutex.Lock()
+	defer requestMutex.Unlock()
+	if request == nil {
+		request = prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Namespace: "kafka",
+				Subsystem: "consumer",
+				Name:      "requests_total",
+				Help:      "Number of requests processed",
+			}, metricLabels)
+		prometheus.MustRegister(request)
+	}
+
+	return request
+}
+
+func getPrometheusLatencyInstrumentation() prometheusSummaryVec {
+	if latency != nil {
+		return latency
+	}
+
+	latencyMutex.Lock()
+	defer latencyMutex.Unlock()
+	if latency == nil {
+		latency = prometheus.NewSummaryVec(
+			prometheus.SummaryOpts{
+				Namespace: "kafka",
+				Subsystem: "consumer",
+				Name:      "requests_latency_milliseconds",
+				Help:      "Total duration in milliseconds",
+			}, metricLabels)
+		prometheus.MustRegister(latency)
+	}
+
+	return latency
 }
 
 // NewConsumerMetricsService creates a layer of service that add metrics capability
-func NewConsumerMetricsService(appName string) *ConsumerMetricsService {
+func NewConsumerMetricsService(groupID string) *ConsumerMetricsService {
 	var c ConsumerMetricsService
-	fieldKeys := []string{"kafka_topic", "success"}
+	c.groupID = groupID
 
-	c.request = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Namespace: "kafka",
-			Subsystem: "consumer",
-			Name:      "requests_total",
-			Help:      "Number of requests processed",
-		}, fieldKeys)
-	prometheus.MustRegister(c.request)
-
-	c.latency = prometheus.NewSummaryVec(
-		prometheus.SummaryOpts{
-			Namespace: "kafka",
-			Subsystem: "consumer",
-			Name:      "requests_latency_milliseconds",
-			Help:      "Total duration in milliseconds",
-		}, []string{"kafka_topic"})
-	prometheus.MustRegister(c.latency)
+	c.request = getPrometheusRequestInstrumentation()
+	c.latency = getPrometheusLatencyInstrumentation()
 
 	return &c
 }
@@ -46,12 +91,9 @@ func (c *ConsumerMetricsService) Instrumentation(next Handler) Handler {
 	return func(ctx context.Context, msg *sarama.ConsumerMessage) (err error) {
 		// add metrics to this method
 		defer func(begin time.Time) {
-			var success bool
-			if err == nil {
-				success = true
-			}
-			c.latency.WithLabelValues(msg.Topic).Observe(time.Since(begin).Seconds() * 1e3)
-			c.request.WithLabelValues(msg.Topic, strconv.FormatBool(success)).Inc()
+			success := strconv.FormatBool(err == nil)
+			c.latency.WithLabelValues(msg.Topic, success, c.groupID).Observe(time.Since(begin).Seconds() * 1e3)
+			c.request.WithLabelValues(msg.Topic, success, c.groupID).Inc()
 		}(time.Now())
 
 		err = next(ctx, msg)
