3ec04f906af8201a2add8a3b0f8ad08c748aa32c
Erwan van Peteghem <erwanvp@gmail.com>
2026-01-26 09:39:40 +0100

    fix data time

--- logger.go
+++ logger.go
@@ -4,32 +4,49 @@ import (
 	"context"
 	"log/slog"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/IBM/sarama"
 )
 
-// componentAttr is the attribute added to all go-kafka logs to identify their source.
-const componentName = "go-kafka"
+// timeFormat is the timestamp format used in logs (millisecond precision).
+const timeFormat = "15:04:05.000"
+
+// defaultHandlerOptions returns the default slog.HandlerOptions with lowercase level
+// and short timestamp format.
+func defaultHandlerOptions(level slog.Level) *slog.HandlerOptions {
+	return &slog.HandlerOptions{
+		Level: level,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			// Convert level to lowercase (info, debug, warn, error)
+			if a.Key == slog.LevelKey {
+				level := a.Value.Any().(slog.Level)
+				a.Value = slog.StringValue(strings.ToLower(level.String()))
+			}
+			// Format time with millisecond precision (HH:MM:SS.mmm)
+			if a.Key == slog.TimeKey {
+				t := a.Value.Time()
+				a.Value = slog.StringValue(t.Format(timeFormat))
+			}
+			return a
+		},
+	}
+}
 
 // DefaultLogger is the default slog.Logger instance used by go-kafka.
 // By default, it logs at Info level to stderr with text format.
-// All logs include a "component" attribute set to "go-kafka".
 // You can replace it with your own logger using SetLogger().
-var DefaultLogger = slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-	Level: slog.LevelInfo,
-})).With("component", componentName)
+var DefaultLogger = slog.New(slog.NewTextHandler(os.Stderr, defaultHandlerOptions(slog.LevelInfo)))
 
 // SetLogger sets a custom slog.Logger for go-kafka.
 // This allows you to use your own logging configuration, handlers, and format.
-// Note: The "component" attribute is NOT automatically added to custom loggers.
-// If you want it, add it yourself: logger.With("component", "go-kafka")
 //
 // Example with JSON handler:
 //
 //	kafka.SetLogger(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 //	    Level: slog.LevelDebug,
-//	})).With("component", "go-kafka"))
+//	})))
 //
 // Example with custom handler:
 //
@@ -39,13 +56,10 @@ func SetLogger(logger *slog.Logger) {
 }
 
 // SetLogLevel sets the minimum log level for the default logger.
-// This creates a new TextHandler with the specified level and preserves
-// the "component" attribute.
+// This creates a new TextHandler with the specified level.
 // For more control, use SetLogger() with a custom handler.
 func SetLogLevel(level slog.Level) {
-	DefaultLogger = slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-		Level: level,
-	})).With("component", componentName)
+	DefaultLogger = slog.New(slog.NewTextHandler(os.Stderr, defaultHandlerOptions(level)))
 }
 
 // MessageContext contains contextual information about a Kafka message for logging.
@@ -96,44 +110,47 @@ func (mc MessageContext) LogAttrs() []slog.Attr {
 
 // --- Convenience functions using the default logger ---
 
+// logPrefix is prepended to all log messages for visibility.
+const logPrefix = "[go-kafka] "
+
 // LogDebug logs a debug message.
 func LogDebug(msg string, args ...any) {
-	DefaultLogger.Debug(msg, args...)
+	DefaultLogger.Debug(logPrefix+msg, args...)
 }
 
 // LogInfo logs an info message.
 func LogInfo(msg string, args ...any) {
-	DefaultLogger.Info(msg, args...)
+	DefaultLogger.Info(logPrefix+msg, args...)
 }
 
 // LogWarn logs a warning message.
 func LogWarn(msg string, args ...any) {
-	DefaultLogger.Warn(msg, args...)
+	DefaultLogger.Warn(logPrefix+msg, args...)
 }
 
 // LogError logs an error message.
 func LogError(msg string, args ...any) {
-	DefaultLogger.Error(msg, args...)
+	DefaultLogger.Error(logPrefix+msg, args...)
 }
 
 // LogDebugContext logs a debug message with context.
 func LogDebugContext(ctx context.Context, msg string, args ...any) {
-	DefaultLogger.DebugContext(ctx, msg, args...)
+	DefaultLogger.DebugContext(ctx, logPrefix+msg, args...)
 }
 
 // LogInfoContext logs an info message with context.
 func LogInfoContext(ctx context.Context, msg string, args ...any) {
-	DefaultLogger.InfoContext(ctx, msg, args...)
+	DefaultLogger.InfoContext(ctx, logPrefix+msg, args...)
 }
 
 // LogWarnContext logs a warning message with context.
 func LogWarnContext(ctx context.Context, msg string, args ...any) {
-	DefaultLogger.WarnContext(ctx, msg, args...)
+	DefaultLogger.WarnContext(ctx, logPrefix+msg, args...)
 }
 
 // LogErrorContext logs an error message with context.
 func LogErrorContext(ctx context.Context, msg string, args ...any) {
-	DefaultLogger.ErrorContext(ctx, msg, args...)
+	DefaultLogger.ErrorContext(ctx, logPrefix+msg, args...)
 }
 
 // --- Message-aware logging functions ---
@@ -150,20 +167,20 @@ func logWithMessageContext(mc MessageContext, args ...any) []any {
 
 // LogMessageDebug logs a debug message with message context.
 func LogMessageDebug(msg string, mc MessageContext, args ...any) {
-	DefaultLogger.Debug(msg, logWithMessageContext(mc, args...)...)
+	DefaultLogger.Debug(logPrefix+msg, logWithMessageContext(mc, args...)...)
 }
 
 // LogMessageInfo logs an info message with message context.
 func LogMessageInfo(msg string, mc MessageContext, args ...any) {
-	DefaultLogger.Info(msg, logWithMessageContext(mc, args...)...)
+	DefaultLogger.Info(logPrefix+msg, logWithMessageContext(mc, args...)...)
 }
 
 // LogMessageWarn logs a warning message with message context.
 func LogMessageWarn(msg string, mc MessageContext, args ...any) {
-	DefaultLogger.Warn(msg, logWithMessageContext(mc, args...)...)
+	DefaultLogger.Warn(logPrefix+msg, logWithMessageContext(mc, args...)...)
 }
 
 // LogMessageError logs an error message with message context.
 func LogMessageError(msg string, mc MessageContext, args ...any) {
-	DefaultLogger.Error(msg, logWithMessageContext(mc, args...)...)
+	DefaultLogger.Error(logPrefix+msg, logWithMessageContext(mc, args...)...)
 }
