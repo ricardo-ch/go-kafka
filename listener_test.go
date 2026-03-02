1e62fe9e8c6665cdee3da07d058e1199cd9e81f3
Erwan van Peteghem <erwanvp@gmail.com>
2026-03-02 09:33:07 +0100

    Move tracing in dedicated files

--- listener_test.go
+++ listener_test.go
@@ -693,45 +693,6 @@ func Test_handleMessageWithRetry_InfiniteRetriesWithContextCancel(t *testing.T) 
 
 }
 
-// Basically a copy paste of the happy path but with tracing
-// This test only checks that the tracing is not preventing the consumption
-func Test_ConsumerClaim_HappyPath_WithTracing(t *testing.T) {
-	msgChanel := make(chan *sarama.ConsumerMessage, 1)
-	msgChanel <- &sarama.ConsumerMessage{
-		Topic: "topic-test",
-	}
-	close(msgChanel)
-
-	consumerGroupClaim := &mocks.ConsumerGroupClaim{}
-	setupConsumerGroupClaimMock(consumerGroupClaim, "topic-test", 0, (<-chan *sarama.ConsumerMessage)(msgChanel))
-
-	consumerGroupSession := &mocks.ConsumerGroupSession{}
-	consumerGroupSession.On("Context").Return(context.Background())
-	consumerGroupSession.On("MarkMessage", mock.Anything, mock.Anything).Return()
-
-	handlerCalled := false
-	handlerProcessor := func(ctx context.Context, msg *sarama.ConsumerMessage) error {
-		handlerCalled = true
-		return nil
-	}
-	handler := Handler{
-		Processor: handlerProcessor,
-		Config:    testHandlerConfig,
-	}
-
-	tested := listener{
-		handlers: map[string]Handler{"topic-test": handler},
-		tracer:   DefaultTracing, // this is the important part
-	}
-
-	err := tested.ConsumeClaim(consumerGroupSession, consumerGroupClaim)
-
-	assert.NoError(t, err)
-	assert.True(t, handlerCalled)
-	consumerGroupClaim.AssertExpectations(t)
-	consumerGroupSession.AssertExpectations(t)
-}
-
 // Test that as long as context is not canceled and not error is returned, `Consume` is called again
 // (when rebalance is called, the consumer will be part of next session)
 func Test_Listen_Happy_Path(t *testing.T) {
---
+++ tracing_test.go
@@ -0,0 +1,74 @@
+package kafka
+
+import (
+	"context"
+	"testing"
+
+	"github.com/IBM/sarama"
+	"github.com/ricardo-ch/go-kafka/v3/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func Test_ConsumerClaim_HappyPath_WithTracing(t *testing.T) {
+	msgChanel := make(chan *sarama.ConsumerMessage, 1)
+	msgChanel <- &sarama.ConsumerMessage{
+		Topic: "topic-test",
+	}
+	close(msgChanel)
+
+	consumerGroupClaim := &mocks.ConsumerGroupClaim{}
+	setupConsumerGroupClaimMock(consumerGroupClaim, "topic-test", 0, (<-chan *sarama.ConsumerMessage)(msgChanel))
+
+	consumerGroupSession := &mocks.ConsumerGroupSession{}
+	consumerGroupSession.On("Context").Return(context.Background())
+	consumerGroupSession.On("MarkMessage", mock.Anything, mock.Anything).Return()
+
+	handlerCalled := false
+	handlerProcessor := func(ctx context.Context, msg *sarama.ConsumerMessage) error {
+		handlerCalled = true
+		return nil
+	}
+	handler := Handler{
+		Processor: handlerProcessor,
+		Config:    testHandlerConfig,
+	}
+
+	tested := listener{
+		handlers: map[string]Handler{"topic-test": handler},
+		tracer:   DefaultTracing,
+	}
+
+	err := tested.ConsumeClaim(consumerGroupSession, consumerGroupClaim)
+
+	assert.NoError(t, err)
+	assert.True(t, handlerCalled)
+	consumerGroupClaim.AssertExpectations(t)
+	consumerGroupSession.AssertExpectations(t)
+}
+
+func Test_GetKafkaHeadersFromContext_EmptyContext(t *testing.T) {
+	headers := GetKafkaHeadersFromContext(context.Background())
+	assert.NotNil(t, headers)
+	// Without TracerProvider configured, propagator is no-op, headers are typically empty
+}
+
+func Test_SerializeKafkaHeadersFromContext_EmptyContext(t *testing.T) {
+	jsonStr, err := SerializeKafkaHeadersFromContext(context.Background())
+	assert.NoError(t, err)
+	assert.NotNil(t, jsonStr)
+	assert.Contains(t, jsonStr, "{")
+}
+
+func Test_DeserializeContextFromKafkaHeaders_InvalidJSON(t *testing.T) {
+	ctx, err := DeserializeContextFromKafkaHeaders(context.Background(), "invalid json")
+	assert.Error(t, err)
+	assert.Nil(t, ctx)
+}
+
+func Test_DeserializeContextFromKafkaHeaders_ValidEmptyJSON(t *testing.T) {
+	ctx, err := DeserializeContextFromKafkaHeaders(context.Background(), "{}")
+	assert.NoError(t, err)
+	assert.NotNil(t, ctx)
+}
+
